675b346c497c683d29e258b2b1980c9516705a2e
ichbintobias2 <t-gaming242@protonmail.com>
2021-06-19 22:13:06 +0200

    introducing PlayerMovementController

--- pokegame-frontend/src/main/java/de/tobias/pokegame/frontend/entities/Player.java
+++ pokegame-frontend/src/main/java/de/tobias/pokegame/frontend/entities/Player.java
@@ -1,47 +1,29 @@
 package de.tobias.pokegame.frontend.entities;
 
-import java.awt.event.KeyEvent;
-
 import de.gurkenlabs.litiengine.Game;
-import de.gurkenlabs.litiengine.IUpdateable;
 import de.gurkenlabs.litiengine.entities.CollisionInfo;
 import de.gurkenlabs.litiengine.entities.Creature;
 import de.gurkenlabs.litiengine.entities.Entity;
 import de.gurkenlabs.litiengine.entities.EntityInfo;
 import de.gurkenlabs.litiengine.entities.IEntity;
 import de.gurkenlabs.litiengine.entities.MovementInfo;
-import de.gurkenlabs.litiengine.entities.Trigger;
-import de.gurkenlabs.litiengine.input.KeyboardEntityController;
 import de.gurkenlabs.litiengine.physics.IMovementController;
 import de.gurkenlabs.litiengine.util.geom.GeometricUtilities;
-import de.tobias.pokegame.backend.wild.EncounterCheck;
-import de.tobias.pokegame.frontend.BattleControl;
 import de.tobias.pokegame.frontend.GameLogic;
 import de.tobias.pokegame.frontend.constants.GameState;
+import de.tobias.pokegame.frontend.entities.controllers.PlayerMovementController;
 
 @EntityInfo(width = 16, height = 16)
 @MovementInfo(velocity = 70)
 @CollisionInfo(collisionBoxWidth = 16, collisionBoxHeight = 16, collision = true)
-public class Player extends Creature implements IUpdateable {
+public class Player extends Creature {
 	
 	private static Player instance;
-	
-	private boolean cooldown = false;
 	
 	private Player() {
 		super("npc_placeholder");
 		
-		KeyboardEntityController<Player> movementController = new KeyboardEntityController<>(this);
-	    movementController.addUpKey(KeyEvent.VK_UP);
-	    movementController.addDownKey(KeyEvent.VK_DOWN);
-	    movementController.addLeftKey(KeyEvent.VK_LEFT);
-	    movementController.addRightKey(KeyEvent.VK_RIGHT);
-	    
-		this.setController(IMovementController.class, movementController);
-		this.getController(IMovementController.class).onMovementCheck(e -> {
-			checkTallGrass();
-			return GameLogic.getState() == GameState.INGAME;
-		});
+		this.setController(IMovementController.class, new PlayerMovementController(this));
 	}
 	
 	public static Player instance() {
@@ -50,11 +32,6 @@ public class Player extends Creature implements IUpdateable {
 		}
 		
 		return instance;
-	}
-
-	@Override
-	public void update() {
-		
 	}
 	
 	public void talkToNPC() {
@@ -66,20 +43,6 @@ public class Player extends Creature implements IUpdateable {
 				.findEntities(GeometricUtilities.extrude(Player.instance().getBoundingBox(), 2))) {
 			if (entity instanceof Entity) {
 				entity.sendMessage(Player.instance(), Entity.ANY_MESSAGE);
-			}
-		}
-	}
-	
-	private void checkTallGrass() {
-		for (IEntity entity : Game.world().environment().findEntities(GeometricUtilities.extrude(Player.instance().getBoundingBox(), 2))) {
-			if (entity instanceof Trigger && entity.getProperties().getBoolValue("grass")) {
-				if (EncounterCheck.isEncountered() && !cooldown) {
-					int id = EncounterCheck.getEncounter("route1"); // TODO
-					BattleControl.startWildBattle(id);
-					
-					// The idea is that this method should only trigger one battle and then wait until it is concluded
-					cooldown = true;
-				}
 			}
 		}
 	}
---
+++ pokegame-frontend/src/main/java/de/tobias/pokegame/frontend/entities/controllers/PlayerMovementController.java
@@ -0,0 +1,73 @@
+package de.tobias.pokegame.frontend.entities.controllers;
+
+import java.awt.event.KeyEvent;
+import java.util.Arrays;
+import java.util.List;
+
+import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.entities.IEntity;
+import de.gurkenlabs.litiengine.entities.IMobileEntity;
+import de.gurkenlabs.litiengine.entities.Trigger;
+import de.gurkenlabs.litiengine.input.KeyboardEntityController;
+import de.gurkenlabs.litiengine.util.geom.GeometricUtilities;
+import de.tobias.pokegame.backend.wild.EncounterCheck;
+import de.tobias.pokegame.frontend.BattleControl;
+import de.tobias.pokegame.frontend.GameLogic;
+import de.tobias.pokegame.frontend.constants.GameState;
+import de.tobias.pokegame.frontend.entities.Player;
+
+public class PlayerMovementController extends KeyboardEntityController<IMobileEntity> {
+	
+	private final List<Integer> up = Arrays.asList(KeyEvent.VK_W);
+	private final List<Integer> down = Arrays.asList(KeyEvent.VK_S);
+	private final List<Integer> left = Arrays.asList(KeyEvent.VK_A);
+	private final List<Integer> right = Arrays.asList(KeyEvent.VK_D);
+	
+	private boolean canEncounter = true;
+	
+	private int movementCooldown = 100;
+	private long lastMovement = 0;
+	
+	public PlayerMovementController(IMobileEntity entity) {
+		super(entity);
+		
+		this.onMovementCheck(e -> {
+			checkTallGrass();
+			return GameLogic.getState() == GameState.INGAME;
+		});
+	}
+	
+	/* TODO this method is commented out because it is not working as intended yet.
+	@Override
+	public void handlePressedKey(KeyEvent e) {
+		float tileSize = 16 * Game.world().camera().getRenderScale(); // Player should move exactly one tile per pressed key
+		
+		if (Game.time().since(lastMovement) > movementCooldown) {
+			if (this.up.contains(e.getKeyCode())) {
+				this.setDy(this.getDy() - tileSize);
+			} else if (this.down.contains(e.getKeyCode())) {
+				this.setDy(this.getDy() + tileSize);
+			} else if (this.left.contains(e.getKeyCode())) {
+				this.setDx(this.getDx() - tileSize);
+			} else if (this.right.contains(e.getKeyCode())) {
+				this.setDx(this.getDx() + tileSize);
+			}
+			
+			lastMovement = Game.time().now();
+		}
+	} */
+	
+	private void checkTallGrass() {
+		for (IEntity entity : Game.world().environment().findEntities(GeometricUtilities.extrude(Player.instance().getBoundingBox(), 2))) {
+			if (entity instanceof Trigger && entity.getProperties().getBoolValue("grass")) {
+				if (EncounterCheck.isEncountered() && canEncounter) {
+					int id = EncounterCheck.getEncounter("route1"); // TODO get name dynamically
+					
+					// The idea is that this method should only trigger one battle and then wait until it is concluded
+					BattleControl.startWildBattle(id);
+					canEncounter = false;
+				}
+			}
+		}
+	}
+}
